a403dd93c76cfb3d8882b315f66e257be3d463f3
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-09-20 16:37:14 +0200

    Add javadoc and sources jar to publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    implementation("com.augustcellars.cose:cose-java:0.9.7")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    implementation("com.augustcellars.cose:cose-java:1.1.0")
     api("com.yubico:webauthn-server-core:2.0.0")
 }
 
@@ -25,11 +25,24 @@ tasks.javadoc {
     (options as StandardJavadocDocletOptions).tags("apiNote:a:API Note:", "implNote:a:Implementation Note:")
 }
 
+val javadocJar = tasks.create<Jar>("javadocJar") {
+    dependsOn(tasks.javadoc)
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc.get().destinationDir)
+}
+
+val sourcesJar = tasks.create<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets[SourceSet.MAIN_SOURCE_SET_NAME].allSource)
+}
+
 publishing {
 
     publications {
         create<MavenPublication>("main") {
             from(components["java"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
         }
     }
 }
