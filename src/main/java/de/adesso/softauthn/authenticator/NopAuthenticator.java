016b9ebd9b4f826896852c7ae62e37193accea63
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-09-22 11:34:20 +0200

    Add skeletons for no-op and broken authenticator implementations

---
+++ src/main/java/de/adesso/softauthn/authenticator/NopAuthenticator.java
@@ -0,0 +1,45 @@
+package de.adesso.softauthn.authenticator;
+
+import com.upokecenter.cbor.CBORObject;
+import com.yubico.webauthn.data.AuthenticatorAttachment;
+import com.yubico.webauthn.data.PublicKeyCredentialDescriptor;
+import com.yubico.webauthn.data.PublicKeyCredentialParameters;
+import com.yubico.webauthn.data.RelyingPartyIdentity;
+import com.yubico.webauthn.data.UserIdentity;
+import de.adesso.softauthn.Authenticator;
+import de.adesso.softauthn.AuthenticatorAssertionData;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+/**
+ * Authenticator implementation that does nothing except throw an error.
+ * This results in it being ignored by {@link de.adesso.softauthn.CredentialsContainer} instances.
+ */
+public class NopAuthenticator implements Authenticator {
+    @Override
+    public CBORObject makeCredential(byte[] hash, RelyingPartyIdentity rpEntity, UserIdentity userEntity, boolean requireResidentKey, boolean requireUserVerification, List<PublicKeyCredentialParameters> credTypesAndPubKeyAlgs, Set<PublicKeyCredentialDescriptor> excludeCredentials, boolean enterpriseAttestationPossible, byte[] extensions) throws IllegalArgumentException, UnsupportedOperationException, IllegalStateException {
+        return null;
+    }
+
+    @Override
+    public AuthenticatorAssertionData getAssertion(String rpId, byte[] hash, List<PublicKeyCredentialDescriptor> allowedCredentialDescriptorList, boolean requireUserVerification, byte[] extensions) throws IllegalArgumentException, NoSuchElementException {
+        return null;
+    }
+
+    @Override
+    public AuthenticatorAttachment getAttachment() {
+        return null;
+    }
+
+    @Override
+    public boolean supportsClientSideDiscoverablePublicKeyCredentialSources() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsUserVerification() {
+        return false;
+    }
+}
